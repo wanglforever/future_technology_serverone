cc2feccefa76aaef6f2353541f41e8108935df3b
wanglei <wanglei@kyee.com.cn>
2017-11-18 16:57:34 +0800

    test

---
+++ ft.serverone/src/main/java/com/future/technology/serverone/config/MyAuthenticationProvider.java
@@ -0,0 +1,12 @@
+package com.future.technology.serverone.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wangl
+ * Date: 2017/10/30
+ * Time: 23:40
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class MyAuthenticationProvider {
+}
---
+++ ft.serverone/src/main/java/com/future/technology/serverone/utils/MD5Util.java
@@ -0,0 +1,12 @@
+package com.future.technology.serverone.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: wangl
+ * Date: 2017/10/30
+ * Time: 21:40
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+public class MD5Util {
+}
